75081eafe362a582a5c702c312fbb156385c8e32
vinomanic <vino1393@gmail.com>
2023-08-24 10:32:31 +0530

    firefox fix

--- apps/admin-app/public/admin-mfe.js
+++ apps/admin-app/public/admin-mfe.js
@@ -1,15 +1,17 @@
-import manifest from "./manifest.json" assert { type: "json" };
-
 export const mountApp = ({ element, options }) => {
   const { baseURL, ...rest } = options;
   if (!baseURL) {
     throw Error(
-      "Please provide the baseURL in the options for the admin MFE to load"
+      'Please provide the baseURL in the options for the admin MFE to load'
     );
   }
-  import(`${baseURL}${manifest["index.html"].file}`).then(() => {
-    const adminMfe = document.createElement("admin-mfe");
-    adminMfe.setAttribute("options", JSON.stringify(rest));
-    element.appendChild(adminMfe);
-  });
+  fetch(`${baseURL}manifest.json`)
+    .then((res) => res.json())
+    .then((data) => {
+      import(`${baseURL}${data['index.html'].file}`).then(() => {
+        const adminMfe = document.createElement('admin-mfe');
+        adminMfe.setAttribute('options', JSON.stringify(rest));
+        element.appendChild(adminMfe);
+      });
+    });
 };
